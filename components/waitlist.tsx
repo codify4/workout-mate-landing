a81ef11e2c564029aebe05b561360b846d94bcae
codify4 <kushta.joni@gmail.com>
2025-05-30 20:20:39 +0200

    added escape chars

--- components/waitlist.tsx
+++ components/waitlist.tsx
@@ -54,7 +54,7 @@ export function Waitlist({ className }: { className?: string }) {
                 <DialogHeader>
                     <DialogTitle className="text-white">Join our waitlist</DialogTitle>
                     <DialogDescription className="text-white/70">
-                        We'll notify you when we launch.
+                        We&apos;re currently accepting applications for our waitlist. If you&apos;re interested, please fill out the form below.
                     </DialogDescription>
                 </DialogHeader>
                 <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center w-full gap-2">
